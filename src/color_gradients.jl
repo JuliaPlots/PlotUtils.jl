2071975ae83bcef356702d403f0237d6b7d8e0d6
Oliver Schulz <oschulz@mpp.mpg.de>
2019-01-25 17:31:23 +0100

    Use global IdDict for misc color library instead of global Vector
    Suggested by @JeffBezanson, should fast and safe for static compilation.

--- src/color_gradients.jl
+++ src/color_gradients.jl
@@ -69,8 +69,7 @@ const _testColors = RGBA{Float64}[colorant"darkblue", colorant"blueviolet",  col
 
 const _gradients = [:Plots]
 
-
-const _misc_color_lib_contents = Pair{Symbol,Vector{RGBA{Float64}}}[
+const _misc_color_lib = ColorLibrary(IdDict(:default => :sequential, :sequential => :heat, :diverging => :bluesreds), IdDict(
     :reds         => RGBA{Float64}[colorant"lightpink", colorant"darkred"],
     :greens       => RGBA{Float64}[colorant"lightgreen", colorant"darkgreen"],
     :redsblues    => RGBA{Float64}[colorant"darkred", RGB(0.8,0.85,0.8), colorant"darkblue"],
@@ -82,23 +81,7 @@ const _misc_color_lib_contents = Pair{Symbol,Vector{RGBA{Float64}}}[
     :darkrainbow  => map(darken, _rainbowColors),
     :darktest     => _testColors,
     :lighttest    => map(c -> lighten(c, 0.3), _testColors),
-]
-
-function _generate_misc_color_lib()
-    lib = Dict{Symbol, Vector{RGBA{Float64}}}()
-    for elem in _misc_color_lib_contents
-        lib[elem[1]] = elem[2]
-    end
-
-    ColorLibrary(
-        Dict{Symbol, Symbol}(
-            :default => :sequential,
-            :sequential => :heat,
-            :diverging => :bluesreds
-        ),
-        lib
-    )
-end
+))
 
 
 """
