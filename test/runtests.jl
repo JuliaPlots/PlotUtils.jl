cc1388506244a0b98da1affa5022f43d6e9dab7d
Miles Lucas <mdlucas@hawaii.edu>
2020-05-09 01:46:57 -1000

    add accuracy test to zscale

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,15 +16,15 @@ const C0 = RGBA{PlotUtils.Colors.N0f8}
 
 @testset "colors" begin
 
-    @test plot_color(nothing) == C(0,0,0,0)
-    @test plot_color(false) == C(0,0,0,0)
+    @test plot_color(nothing) == C(0, 0, 0, 0)
+    @test plot_color(false) == C(0, 0, 0, 0)
     @test_throws ErrorException plot_color(true)
 
     @test plot_color(:red) == parse(C, :red)
     @test plot_color("red") == parse(C, "red")
     @test_throws ErrorException plot_color("notacolor")
 
-    @test plot_color(colorant"red") == C(1,0,0,1)
+    @test plot_color(colorant"red") == C(1, 0, 0, 1)
 
     grad = cgrad()
     @test typeof(grad) == PlotUtils.ContinuousColorGradient
@@ -32,11 +32,11 @@ const C0 = RGBA{PlotUtils.Colors.N0f8}
 
     grad = cgrad([:red, "blue"])
     @test color_list(grad) == C[colorant"red", colorant"blue"]
-    @test grad.values == collect(range(0,stop=1,length=2))
+    @test grad.values == collect(range(0, stop = 1, length = 2))
 
     grad = cgrad([:red, "blue"], alpha = 0.5)
-    @test C0.(color_list(grad)) == C0[C(1,0,0,0.5), C(0,0,1,0.5)]
-    @test grad.values == collect(range(0,stop=1,length=2))
+    @test C0.(color_list(grad)) == C0[C(1, 0, 0, 0.5), C(0, 0, 1, 0.5)]
+    @test grad.values == collect(range(0, stop = 1, length = 2))
 
     grad = cgrad([:red,:blue], [0,0.1,1])
     @test length(color_list(grad)) == 3
@@ -46,10 +46,10 @@ const C0 = RGBA{PlotUtils.Colors.N0f8}
     @test typeof(cs) == Vector{C}
     @test length(cs) == 10
 
-    cs = plot_color(rand(4,4))
+    cs = plot_color(rand(4, 4))
     @test typeof(cs) == Matrix{C}
     @test length(cs) == 16
-    @test size(cs) == (4,4)
+    @test size(cs) == (4, 4)
 
 
     cs = plot_color(rand(10), 0.5)
@@ -59,10 +59,10 @@ const C0 = RGBA{PlotUtils.Colors.N0f8}
         @test alpha(c) == 0.5
     end
 
-    cs = plot_color(rand(4,4), 0.5)
+    cs = plot_color(rand(4, 4), 0.5)
     @test typeof(cs) == Matrix{C}
     @test length(cs) == 16
-    @test size(cs) == (4,4)
+    @test size(cs) == (4, 4)
     for c in cs
         @test alpha(c) == 0.5
     end
@@ -83,23 +83,21 @@ end
 
 # Copied from Plots.is_uniformly_spaced to avoid dependency on recent version
 # on Plots which is not used on Travis.
-function is_uniformly_spaced(v; tol=1e-6)
-  dv = diff(v)
-  maximum(dv) - minimum(dv) < tol * mean(abs.(dv))
+function is_uniformly_spaced(v; tol = 1e-6)
+    dv = diff(v)
+    maximum(dv) - minimum(dv) < tol * mean(abs.(dv))
 end
 
 @testset "ticks" begin
-    @test optimize_ticks(-1,2) == ([-1.0,0.0,1.0,2.0],-1.0,2.0)
+    @test optimize_ticks(-1, 2) == ([-1.0,0.0,1.0,2.0], -1.0, 2.0)
     dt1, dt2 = Dates.value(DateTime(2000)), Dates.value(DateTime(2100))
-    @test optimize_datetime_ticks(dt1, dt2) == (
-        [63113990400000, 63902908800000, 64691827200000, 65480745600000],
-        ["2001-01-01", "2026-01-01", "2051-01-01", "2076-01-01"]
-    )
+    @test optimize_datetime_ticks(dt1, dt2) == ([63113990400000, 63902908800000, 64691827200000, 65480745600000],
+        ["2001-01-01", "2026-01-01", "2051-01-01", "2076-01-01"])
 
     @testset "small range" begin
         @testset "small range $x, $(i)ϵ" for x in exp10.(-12:12), i in -5:5
-            y = x + i*eps(x)
-            x,y = minmax(x,y)
+            y = x + i * eps(x)
+            x, y = minmax(x, y)
             ticks = PlotUtils.optimize_ticks(x, y)[1]
             @test issorted(ticks)
             @test all(x .<= ticks .<= y)
@@ -118,15 +116,15 @@ end
     end
 
     @testset "fixed ranges" begin
-        @testset "fixed range $x..$y" for (x,y) in [(2,14),(14,25),(16,36),(57,69)]
+        @testset "fixed range $x..$y" for (x, y) in [(2, 14),(14, 25),(16, 36),(57, 69)]
             test_ticks(x, y, optimize_ticks(x, y)[1])
             test_ticks(-y, -x, optimize_ticks(-y, -x)[1])
         end
     end
 
     @testset "random ranges" begin
-        r = [minmax(rand(-100:100,2)...) .* 10.0^i for _=1:10, i=-5:5]
-        @testset "random range $x..$y" for (x,y) in r
+        r = [minmax(rand(-100:100, 2)...) .* 10.0^i for _ = 1:10, i = -5:5]
+        @testset "random range $x..$y" for (x, y) in r
             test_ticks(x, y, optimize_ticks(x, y)[1])
         end
     end
@@ -137,10 +135,10 @@ end
     end
 
     @testset "digits" begin
-        @testset "digits $((10^n)-1)*10^$i" for n in 1:9, i in -9:9
+        @testset "digits $((10^n) - 1)*10^$i" for n in 1:9, i in -9:9
             y0 = 10^n
-            x0 = y0-1
-            x, y = (x0,y0) .* 10.0^i
+            x0 = y0 - 1
+            x, y = (x0, y0) .* 10.0^i
             ticks = optimize_ticks(x, y)[1]
             test_ticks(x, y, ticks)
         end
@@ -159,8 +157,8 @@ end
         for λ in 0:0.1:1
             # test that `f` is well approximated by a line
             # in the interval `(xs[i], xs[i+1])`
-            x = λ * xs[i] + (1 - λ) * xs[i+1]
-            y = λ * fs[i] + (1 - λ) * fs[i+1]
+            x = λ * xs[i] + (1 - λ) * xs[i + 1]
+            y = λ * fs[i] + (1 - λ) * fs[i + 1]
             @test y ≈ f(x) atol = 1e-2
         end
     end
@@ -176,12 +174,17 @@ end
 
 @testset "zscale" begin
     #= this test is useless right now because it doesn't actually
-       modify the clims at all + issues with randn changes in 1.5=#
-    # data = 5 .* randn(100, 100) .+ 10
-    # cmin, cmax = zscale(data)
-    # # values calculated using IRAF
-    # @test cmin ≈ -4.89 atol=0.01
-    # @test cmax ≈ 25.25 atol=0.01
+       modify the clims at all + issues with randn changes in 1.5 =#
+    bkg = 30 .* randn(8192) .+ 1000
+    defects = rand(CartesianIndices(bkg), 500)
+    @. bkg[defects] = rand(Bool) ? 0 : 1e7
+    data = bkg .+ 100 .* randn(8192) .+ 2500
+    cmin, cmax = zscale(data)
+    # values calculated using IRAF
+    @test cmin ≈ 3215.963 atol = 1e-3
+    @test cmax ≈ 4176.060 atol = 1e-3
+    @test cmin > minimum(data)
+    @test cmax < maximum(data)
 
     data = vcat(1:100)
     cmin, cmax = zscale(data)
