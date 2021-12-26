5a53e3533c28bdd036f55c410881ed03a715fe42
t-bltg <t-bltg@users.noreply.github.com>
2021-12-26 00:37:44 +0000

    Format .jl files [skip ci]

--- test/runtests.jl
+++ test/runtests.jl
@@ -153,7 +153,7 @@ end
 
     @testset "issues" begin
         @testset "PlotUtils.jl/issues/86" begin
-            let x = -1., y = 13.
+            let x = -1.0, y = 13.0
                 test_ticks(x, y, optimize_ticks(x, y, k_min = 4, k_max = 8)[1])
             end
         end
@@ -164,7 +164,7 @@ end
         end
 
         @testset "PlotUtils.jl/issues/114" begin
-            let x = -.1eps(), y = .1eps()
+            let x = -0.1eps(), y = 0.1eps()
                 test_ticks(x, y, optimize_ticks(x, y)[1])
             end
         end
@@ -180,7 +180,7 @@ end
 
         @testset "PlotUtils.jl/issues/129" begin
             # invalid float input
-            let x = NaN, y = 1.
+            let x = NaN, y = 1.0
                 ticks, = optimize_ticks(x, y)
                 @test isnan(ticks[1])
                 @test ticks[2] === y
@@ -188,12 +188,12 @@ end
                 @test isnan(ticks[1])
                 @test ticks[2] === y
             end
-            let x = 0f0, y = Inf32
+            let x = 0.0f0, y = Inf32
                 ticks, = optimize_ticks(x, y)
-                @test ticks[1] === x 
+                @test ticks[1] === x
                 @test isinf(ticks[2])
                 ticks, = optimize_ticks(x, y, k_min = 5)
-                @test ticks[1] === x 
+                @test ticks[1] === x
                 @test isinf(ticks[2])
             end
         end
