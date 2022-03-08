49e8c2204f8c8f0010954dc8d1e747e514f24caf
t-bltg <tf.bltg@gmail.com>
2022-03-08 22:50:21 +0100

    disable ticks precompilation

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,5 +1,6 @@
 function _precompile_()
     ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
+    #=
     @assert precompile(
         Tuple{
             Core.kwftype(typeof(optimize_ticks)),
@@ -9,6 +10,9 @@ function _precompile_()
             Float64,
         },
     )
+    @assert precompile(optimize_ticks, (Int, Int))
+    @assert precompile(optimize_datetime_ticks, (Int, Int))
+    =#
     for C in (RGB, RGBA), T in (Colors.FixedPointNumbers.N0f8, Float32, Float64)
         @assert precompile(cgrad, (ColorScheme{Vector{C{T}},String,String},))
         for V in (Vector{Float64}, typeof(get_range(3)))
@@ -26,7 +30,5 @@ function _precompile_()
     for T in (Float64, Int)
         @assert precompile(zscale, (Vector{T},))
     end
-    @assert precompile(optimize_ticks, (Int, Int))
-    @assert precompile(optimize_datetime_ticks, (Int, Int))
     @assert precompile(adapted_grid, (Function, Tuple{Int,Int}))
 end
